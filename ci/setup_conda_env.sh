368e7fc11ca18d010e95cc7bebf0202107a48313
jingxinfu <jingxinfu.tj@gmail.com>
2020-02-10 10:55:05 -0500

    Update setup_conda_env.sh

--- ci/setup_conda_env.sh
+++ ci/setup_conda_env.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# License           : GPL3
+# Author            : Jingxin Fu <jingxinfu.tj@gmail.com>
+# Date              : 10/02/2020
+# Last Modified Date: 10/02/2020
+# Last Modified By  : Jingxin Fu <jingxinfu.tj@gmail.com>
 
 echo "Creating a Python $PYTHON_VERSION environment"
 conda create -n hosts python=$PYTHON_VERSION || exit 1
-source activate hosts
+conda activate hosts
