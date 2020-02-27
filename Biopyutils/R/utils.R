3ef10fe32212ec2afbd79ea320fdf836aacffa04
jingxinfu <jingxinfu.tj@gmail.com>
2020-02-27 16:45:28 -0500

    Bug Fix: BiocManager installation

--- Biopyutils/R/utils.R
+++ Biopyutils/R/utils.R
@@ -1,13 +1,17 @@
 #!/usr/bin/Rscript
 # Author            : Jingxin Fu <jingxinfu.tj@gmail.com>
 # Date              : 14/02/2020
-# Last Modified Date: 20/02/2020
+# Last Modified Date: 27/02/2020
 # Last Modified By  : Jingxin Fu <jingxinfu.tj@gmail.com>
 
 load_package = function(pkgs){
-    if(!is.element('BiocManager', installed.packages()[,1])){
-            install.packages('BiocManager',repos = "http://cran.us.r-project.org")
+    if (!requireNamespace("BiocManager", quietly = TRUE)){
+            install.packages("BiocManager")
+            BiocManager::install(version = "3.10")
     }
+    #if(!is.element('BiocManager', installed.packages()[,1])){
+    #        install.packages('BiocManager',repos = "http://cran.us.r-project.org")
+    #}
     for(el in pkgs){
             if (!is.element(el, installed.packages()[,1])){
                 BiocManager::install(el)
